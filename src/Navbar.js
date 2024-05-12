7e1eb3ebc81383f121f434998614e8a96ced81c4
Omar Faraj <omarfaraj355@gmail.com>
2024-05-12 20:43:45 +0300

    small changes

---
+++ src/Navbar.js
@@ -0,0 +1,10 @@
+import { Link } from 'react-router-dom';
+const Navbar = () => {
+    return ( 
+        <>
+        <li><Link to="/">Home</Link></li>
+        </>
+     );
+}
+ 
+export default Navbar;
